cfb215514f46cb989f34983fd0fc5f15df4017ef
Piotr Żabówka <wooboox@gmail.com>
2026-01-17 21:43:51 +0100

    refactor: use single Bitwarden note 'dotfiles-secrets' for all configuration steps

--- steps/05-secrets.sh
+++ steps/05-secrets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Global dotfiles-secrets already handled in steps/01-secrets-helper.sh or individual steps
+# If you have general templates not tied to a specific app, put them in templates/global
 TEMPLATE_DIR="$REPO_ROOT/templates/global"
 
 process_bw_templates "dotfiles-secrets" "$TEMPLATE_DIR"
